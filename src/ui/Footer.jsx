b4c8459cc0a79dba98fb5d77094fe94eb1cf3f3d
Miguel Ángel <txrz56rad@live.com.mx>
2024-07-08 23:27:23 -0600

    Footer Desk

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -2,7 +2,24 @@ function Footer() {
   return (
     <footer>
       <div className="mx-auto max-w-[1170px] px-[20px] py-[75pt]">
-        hello footer
+        <ul className="flex justify-between">
+          <li>
+            <div>
+              <h2 className="mb-[20px] mt-[13px] font-bold">About Us</h2>
+            </div>
+            <ul className="footer_links">
+              <li>About Us Overview</li>
+              <li>Leadership Team</li>
+              <li>Values In Action</li>
+              <li>Franchising info</li>
+              <li>Recalls & Alerts</li>
+              <li>Real Estate</li>
+              <li>Accessibility</li>
+              <li>Investor Relations</li>
+              <li>News & Notifications</li>
+            </ul>
+          </li>
+        </ul>
       </div>
     </footer>
   );
